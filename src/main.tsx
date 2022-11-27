3d04d5066e30b616806f3248989a9f43e88ff54a
Ryohei Ikegami <iofg2100@gmail.com>
2022-11-27 18:29:22 +0900

    Push updates

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,10 @@ import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
 import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
 import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
 import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
+import { Awareness } from "y-protocols/awareness";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onChildAdded, push, get } from "firebase/database";
+import { Buffer } from "buffer";
 import "./index.css";
 
 // @ts-ignore
@@ -27,6 +31,19 @@ self.MonacoEnvironment = {
   },
 };
 
+const firebaseConfig = {
+  apiKey: "AIzaSyChTWSFwlBcBMT-nQMIdZ_PRbo1LYRD1mM",
+  authDomain: "yjs-realtime-database.firebaseapp.com",
+  databaseURL: "https://yjs-realtime-database-default-rtdb.firebaseio.com",
+  projectId: "yjs-realtime-database",
+  storageBucket: "yjs-realtime-database.appspot.com",
+  messagingSenderId: "302334531831",
+  appId: "1:302334531831:web:b6d9864c5f0b7fe0155bf2",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
 window.addEventListener("load", () => {
   const ydoc = new Y.Doc();
   // const provider = new WebsocketProvider(
@@ -35,6 +52,21 @@ window.addEventListener("load", () => {
   //   ydoc
   // );
   const ytext = ydoc.getText("monaco");
+
+  const awareness = new Awareness(ydoc);
+
+  get(ref(database, "doc")).then((snapshot) => {
+    console.log(snapshot.val());
+  });
+
+  onChildAdded(ref(database, "doc"), (data) => {
+    //Y.applyUpdate(ydoc, data.val());
+  });
+
+  ydoc.on("update", (update: Uint8Array) => {
+    const updateBase64 = Buffer.from(update).toString("base64");
+    push(ref(database, "doc"), updateBase64);
+  });
 
   const editor = monaco.editor.create(
     /** @type {HTMLElement} */ document.getElementById("monaco-editor")!,
@@ -47,8 +79,8 @@ window.addEventListener("load", () => {
   const monacoBinding = new MonacoBinding(
     ytext,
     /** @type {monaco.editor.ITextModel} */ editor.getModel()!,
-    new Set([editor])
-    //provider.awareness
+    new Set([editor]),
+    awareness
   );
 
   const connectBtn =
@@ -64,5 +96,5 @@ window.addEventListener("load", () => {
   });
 
   // @ts-ignore
-  window.example = { provider, ydoc, ytext, monacoBinding };
+  window.example = { ydoc, ytext, monacoBinding };
 });
